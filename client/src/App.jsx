37a5352eab144a432e3dc4687b1fa62aa14c19fc
Fabian <fabianvillagra28@gmail.com>
2025-10-02 20:54:14 -0300

    fix: se elimino el archivo 404

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import PantallaTurnos from './components/PantallaTurnos'
 import PantallaOperador from './components/PantallaOperador'
 import './App.css'
 
+function AppContent() {
+  const location = useLocation()
+  
+  // Debug: mostrar la ruta actual
+  console.log('Ruta actual:', location.pathname)
+  
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<PantallaTurnos />} />
+        <Route path="/operador" element={<PantallaOperador />} />
+        {/* Ruta catch-all para debugging */}
+        <Route path="*" element={
+          <div style={{padding: '20px', textAlign: 'center'}}>
+            <h2>Ruta no encontrada: {location.pathname}</h2>
+            <p><a href="/">Ir a Turnos</a> | <a href="/operador">Ir a Operador</a></p>
+          </div>
+        } />
+      </Routes>
+    </div>
+  )
+}
+
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<PantallaTurnos />} />
-          <Route path="/operador" element={<PantallaOperador />} />
-        </Routes>
-      </div>
+      <AppContent />
     </Router>
   )
 }
