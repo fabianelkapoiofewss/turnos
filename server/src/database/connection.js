cde7f2e50b6ef3a0fbcea7c9b1c07b96936c8533
fabianelkapoiofewss <fabianipf17@gmail.com>
2026-01-20 09:00:56 -0300

    Update connection.js

--- server/src/database/connection.js
+++ server/src/database/connection.js
@@ -3,7 +3,7 @@ import { sequelize } from "./config.js";
 
 export const connectDB = async () => {
     try {
-        await sequelize.sync({ alter: true });
+        await sequelize.sync();
         console.log("Base de datos conectada");
     } catch (error) {
         console.error("Error al conectar a la base de datos:", error);
